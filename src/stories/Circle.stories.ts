923604de6fc09dd9489db0eec30e0fac846b7516
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-07-26 00:23:06 +0900

    オレンジ、緑、黄色のサークルを選択できるようにする

--- src/stories/Circle.stories.ts
+++ src/stories/Circle.stories.ts
@@ -1,12 +1,35 @@
-import { Meta } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import Circle from "./Circle";
 
 const meta: Meta<typeof Circle> = {
-    component: Circle,
-    title:"Circle",
+  component: Circle,
+  title: "Circle",
+  argTypes: {
+    variant: {
+        control: {type: "select"},
+        options:["orange","green","yellow"],
+    },
+  },
 };
 
 export default meta;
 
-export const BaseCircle = {};
+type Story = StoryObj<typeof Circle>;
 
+export const OrangeCircle: Story = {
+  args: {
+    variant: "orange",
+  },
+};
+
+export const GreenCircle: Story = {
+  args: {
+    variant: "green",
+  },
+};
+
+export const YellowCircle: Story = {
+  args: {
+    variant: "yellow",
+  },
+};
